9ad9af9345f9e649ac9b021005312ed06c7d3211
VLola <valiklolanew@gmail.com>
2022-11-07 10:53:36 +0200

    fixed bugs

---
+++ Project_72/Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_72.Repositories;
+using System.Net;
+
+namespace Project_72.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : Controller
+    {
+
+        FileRepo repo;
+        public FileController()
+        {
+            repo = new FileRepo();
+        }
+        [HttpPost("Add")]
+        public HttpStatusCode OnPostUploadAsync(IFormFile file)
+        {
+            FileInfo fi = new FileInfo(file.FileName);
+            if (file.Length > 0)
+            {
+                using (var stream = System.IO.File.Create(repo.RandomName(fi.Extension)))
+                {
+                    file.CopyToAsync(stream);
+                }
+            }
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ Project_72/Repositories/FileRepo.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Project_72.Repositories
+{
+    public class FileRepo
+    {
+        public FileRepo()
+        {
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+        }
+        string path = Directory.GetCurrentDirectory() + "/www/Files/";
+        public string RandomName(string extenc)
+        {
+            while (true)
+            {
+                string name = Path.GetRandomFileName();
+                string fileName = path + name + extenc;
+                if (!File.Exists(fileName))
+                {
+                    return fileName;
+                }
+            }
+        }
+    }
+}
